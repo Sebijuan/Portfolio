ec7b06fccd8c70661de8fbd9c8a2c69debc917cc
Sebijuan <sjuacoc2t@adaits.es>
2025-03-03 18:32:33 +0100

    actualizacion

--- proyecto/src/Componentes/Home/Herosection.jsx
+++ proyecto/src/Componentes/Home/Herosection.jsx
@@ -9,7 +9,6 @@ function HeroSection() {
                 <h2>Desarrollador de Aplicaciones Web</h2>
                 <section id="hero">
                     <img src="/imagenes/yo.jpg" alt="Hero" className="hero-image animated-image" /> {/* Fuente de imagen corregida y clase de animación añadida */}
-                    
                 </section>
             </div>
             <section id="estudios-trabajos">
@@ -18,15 +17,25 @@ function HeroSection() {
                     <div className="estudios-trabajos-content">
                         <div className="estudios">
                             <h3>Estudios</h3>
-                            <p>
-                                He completado el Bachillerato Tecnológico, lo que me ha proporcionado una sólida base en ciencias y tecnología. Actualmente, estoy cursando un Grado Superior de Desarrollo de Aplicaciones Web, donde estoy adquiriendo habilidades avanzadas en programación y desarrollo web.
-                            </p>
+                            <div className="estudio-item">
+                                <h4>Bachillerato Tecnológico</h4>
+                                <p>Proporcionó una sólida base en ciencias y tecnología.</p>
+                            </div>
+                            <div className="estudio-item">
+                                <h4>Grado Superior de Desarrollo de Aplicaciones Web</h4>
+                                <p>Adquiriendo habilidades avanzadas en programación y desarrollo web.</p>
+                            </div>
                         </div>
                         <div className="trabajos">
                             <h3>Trabajos</h3>
-                            <p>
-                                He trabajado como Teleoperador en la empresa Konecta, donde desarrollé habilidades de comunicación y atención al cliente. Además, he trabajado como camarero de boda en Hacienda el Vizir, demostrando mi capacidad para trabajar en equipo y ofrecer un excelente servicio al cliente.
-                            </p>
+                            <div className="trabajo-item">
+                                <h4>Teleoperador en Konecta</h4>
+                                <p>Desarrollé habilidades de comunicación y atención al cliente.</p>
+                            </div>
+                            <div className="trabajo-item">
+                                <h4>Camarero de boda en Hacienda el Vizir</h4>
+                                <p>Demostré mi capacidad para trabajar en equipo y ofrecer un excelente servicio al cliente.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
