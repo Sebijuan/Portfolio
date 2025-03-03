8d0db018799c113f8f836adac20bf034963a3e27
Sebijuan <sjuacoc2t@adaits.es>
2025-03-03 20:51:12 +0100

    actualizacion

--- proyecto/src/Componentes/Layout/navbar.jsx
+++ proyecto/src/Componentes/Layout/navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Estilos/paginaprincipal.css';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleHomeClick = (event) => {
         event.preventDefault();
@@ -25,8 +26,6 @@ const Navbar = () => {
         window.location.href = '/contacto';
     };
 
-    
-
     const handleHabilidadesClick = (event) => {
         event.preventDefault();
         window.location.href = '/habilidades';
@@ -37,18 +36,23 @@ const Navbar = () => {
         window.location.href = '/blog';
     };
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="navbar navbar-visible">
-            <ul>
+            <div className={`hamburger-menu ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                &#9776; {/* Icono de menú hamburguesa */}
+            </div>
+            <ul className={menuOpen ? 'active' : ''}>
                 <li><a href="#home" onClick={handleHomeClick} className="navbar-link">Inicio</a></li>
                 <li><a href="/sobremi" onClick={handleAboutClick} className="navbar-link">Sobre Mi</a></li>
                 <li><a href="/proyectos" onClick={handleProyectosClick} className="navbar-link">Proyectos</a></li>
                 <li><a href="/blog" onClick={handleBlogClick} className="navbar-link">Blog</a></li>
                 <li><a href="/habilidades" onClick={handleHabilidadesClick} className="navbar-link">Habilidades</a></li>
                 <li><a href="/contacto" onClick={handleContactClick} className="navbar-link">Contacto</a></li>
-            </ul> 
-             
-
+            </ul>
         </nav>
     );
 };
